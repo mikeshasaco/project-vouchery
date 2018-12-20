6c949e76e2ed452ab0d497ae04b37e6bb1cda1a5
Michael Shasanya <shasanyam1@students.rowan.edu>
2018-12-20 01:31:37 -0500

    removed the database seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,22 +11,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Faker\Factory::create();
-        for($i=0; $i < 100; $i++){
-            App\Product::create([
-                'user_id' => 1,
-                'title' => $faker->title,
-                'desc' => str_random(50),
-                'image' => public_path('fryer.jpg',240,180),
-                'category_id' => 1,
-                'currentprice' => 300,
-                'newprice' => 100,
-                'couponcode' => 'abiwefa',
-                'advertboolean' => 0,
-                'url' => 'https://www.google.com/',
-                'clicks' => 0
-
-            ]);
-        }
+   
     }
 }
