6033068e7167005bbe69bb8206a2ecd1052d3595
Michael Shasanya <shasanyam1@students.rowan.edu>
2018-12-29 15:24:37 -0500

    changes toward pagination

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
-
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -11,6 +10,25 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-   
+      $faker = Faker\Factory::create();
+
+      for($i = 0; $i < 100; $i++){
+          App\Product::create([
+            'user_id' => 1,
+            'title' => $faker->title,
+            'desc' => str_random(25),
+            'image' => "/ps4.jpg",
+            'currentprice' => 199,
+            'newprice' => 90,
+            'category_id' => 1,
+            'couponcode' => null,
+            'advertboolean' => 0,
+            'clicks' => 0 ,
+            'expired_date' => Carbon::now(),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+            'url' => 'https://www.google.com'
+          ]);
+      }
     }
 }
