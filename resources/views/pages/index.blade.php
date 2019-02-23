e62b7084e0864a68a471dbf396624b3354c18eca
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-23 07:54:50 -0500

    changes shit again

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -143,7 +143,7 @@
                      <p  style="font-weight:bold; font-size:10px; opacity:0.8; margin:0; cursor:pointer;"><i class="far fa-eye icon-battery-percent" title="Clicks/PerView"><b> {{$product->clicks}}</b></i></p>
                            
                 </div>
-                <img class="card-img-bottom" src="https://vouch.sfo2.digitaloceanspaces.com/home/forge/default/storage/app/public/public/images/{{$product->image}}" alt="" height="283" width="180">
+                <img class="card-img-bottom" src="https://vouch.sfo2.digitaloceanspaces.com/home/forge/default/storage/app/public/public/images{{$product->image}}" alt="" height="283" width="180">
                     {{-- <img class="card-img-bottom" src="/images/{{ $product->image }}" height="283" width="180"> --}}
                     @if(auth::user() || auth::guard('customer')->user())
                     <a href="{{$product->url}}" target="_blank" class="cardbutton-page"> View Deal
