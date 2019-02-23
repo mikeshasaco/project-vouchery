0e38cc1c28a48a63cbb65d31e5d0296bfd366f34
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-22 21:09:53 -0500

    fixes

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -176,7 +176,7 @@ class ProductsController extends Controller
             // Storage::disk('do')->setVisibility($path, 'public');
             $image = $request->file('image');
             $filename = time() . '.' . $image->getClientOriginalExtension();
-            $orientate = Image::make($image)->orientate()->save('public/images' . $filename);
+            $orientate = Image::make($image)->orientate()->save(storage_path('public/images' . $filename));
 
             $location = Storage::disk('do')->putFileAs('public/images', $image, $filename);
             Storage::disk('do')->setVisibility($location, 'public');
