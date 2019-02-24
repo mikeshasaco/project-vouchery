67491303339331908eefb530649e28d7b5f8bb5a
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-24 09:20:50 -0500

    fixing shit

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,5 +1,4 @@
 <footer class="border-top d-none d-lg-block" id="footer">
-    <span class="foot"><a href={{ route('help.quest') }}>Help</a></span>
-    <span style="">Copyright &copy; {{ date('Y') }} Mike All Rights Reserved.</span>
+    <span style="">Copyright &copy; {{ date('Y') }} VoucheryHub All Rights Reserved.</span>
 </footer>
 
