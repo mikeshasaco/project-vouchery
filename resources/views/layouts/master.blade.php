ab379ed9917e1034c1e38eb78a6629ab93495e4d
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-26 23:28:04 -0500

    added google adsense

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,14 @@
 
 {{--  tab icon--}}
 	<link rel="shortcut icon" href="/vouchtab.png">
-
+		
+	<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+		<script>
+		(adsbygoogle = window.adsbygoogle || []).push({
+			google_ad_client: "ca-pub-1561005604675174",
+			enable_page_level_ads: true
+		});
+		</script>
 
 	<!--bootstrap  -->
 	<link href="{{ asset('css/mobilenavbarcustom.css') }}" rel="stylesheet">
