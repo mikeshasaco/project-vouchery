63beb428c588fd3d8a6745c3d8121c0174ebb34a
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-28 15:14:12 -0500

    removed the env stuff

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -20,7 +20,7 @@
   <div class="carousel-inner" role="listbox">
     @foreach( $submission as $photo )
        <div class="carousel-item {{ $loop->first ? 'active' : '' }}">
-       <a href="{{$photo->weblink}}" target="_blank"> <img  src="{{env('VH')}}{{$photo->image}}" width="100%;" height="280px;" >
+       <a href="{{$photo->weblink}}" target="_blank"> <img  src="https://vouch.sfo2.digitaloceanspaces.com/home/forge/voucheryhub.com/storage/app/public/Banner/{{$photo->image}}" width="100%;" height="280px;" >
        </a>
 
        </div>
