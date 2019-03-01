bede504df386abe0b13b2db12d4b146420c77eb7
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-03-01 18:01:27 -0500

    changed string to text

--- database/migrations/2018_04_17_024059_add_url_to_products.php
+++ database/migrations/2018_04_17_024059_add_url_to_products.php
@@ -14,7 +14,7 @@ class AddUrlToProducts extends Migration
     public function up()
     {
         Schema::table('products', function (Blueprint $table) {
-            $table->string('url');
+            $table->text('url');
         });
     }
 
