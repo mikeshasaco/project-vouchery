a1137e50599177b644e39935b51ad59b28000f2c
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-02-28 21:29:29 -0500

    fixing stripe

--- resources/views/email/receipt.blade.php
+++ resources/views/email/receipt.blade.php
@@ -1,14 +1,14 @@
 @component('mail::message')
 # Order Receipt
 
-Your Coupon Advertisement is Running!, Coupon last up to 7 days from when the coupon was originally created. So Advertisements delete once the original 7 days are up.<br>
+Your Coupon Advertisement is Running, Coupon lasts up to 7 days from when the coupon was originally created. So Advertisements will delete once the original 7 days are up.<br>
 @component('mail::table')
 | Advertisement       | CouponName         | Price  |
 |: ------------- :|:-------------:| --------:|
 | <h3 style="color:green;">Running</h3>       | {{ $advertisement->adname}}       | {{ $advertisement->adprice }}  |
 @endcomponent
 
-@component('mail::button', ['url' => 'http://localhost:8000/', 'color' => 'red'])
+@component('mail::button', ['url' => 'https://www.voucheryhub.com', 'color' => 'red'])
 VoucheryHub
 @endcomponent
 
