2c14abcca67ad51c780d96fada8bf2e72948c36c
Michael Shasanya <shasanyam1@students.rowan.edu>
2019-10-02 07:47:51 -0400

    changed facebook code

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -23,7 +23,7 @@
 		});
 		</script>
 
-		<!-- Facebook Pixel Code -->
+<!-- Facebook Pixel Code -->
 <script>
   !function(f,b,e,v,n,t,s)
   {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
@@ -33,11 +33,11 @@
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '393053994823891');
+  fbq('init', '387119268900181');
   fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=393053994823891&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=387119268900181&ev=PageView&noscript=1"
 /></noscript>
 <!-- End Facebook Pixel Code -->
 
