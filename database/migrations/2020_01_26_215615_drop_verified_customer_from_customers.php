1abcbfc48b6f0cfe63b07b4c030243a7cb45bf2d
Michael Shasanya <shasanyam1@students.rowan.edu>
2020-01-26 16:59:41 -0500

    deleted verifed for customers

--- database/migrations/2020_01_26_215615_drop_verified_customer_from_customers.php
+++ database/migrations/2020_01_26_215615_drop_verified_customer_from_customers.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class DropVerifiedCustomersFromCustomers extends Migration
+class DropVerifiedCustomerFromCustomers extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,9 @@ class DropVerifiedCustomersFromCustomers extends Migration
      */
     public function up()
     {
-        //
+        Schema::table('customers', function (Blueprint $table) {
+            $table->dropColumn('verified');
+        });
     }
 
     /**
@@ -23,6 +25,8 @@ class DropVerifiedCustomersFromCustomers extends Migration
      */
     public function down()
     {
-        //
+        Schema::table('customers', function (Blueprint $table) {
+            $table->boolean('verified');
+        });
     }
 }
