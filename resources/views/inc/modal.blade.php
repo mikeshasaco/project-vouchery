7eeb108928189bde3c6aeb43ccecd484bab5ad5e
Michael Shasanya <shasanyam1@students.rowan.edu>
2020-02-01 20:30:06 -0500

    removing multiple clicks

--- resources/views/inc/modal.blade.php
+++ resources/views/inc/modal.blade.php
@@ -1,7 +1,7 @@
 <div class="modal fade form-prevent-multiple-submits" id="productmodal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
   <div class="modal-dialog" role="document">
     <div class="modal-content">
-      <form method="post" enctype="multipart/form-data" action="{{route('product.store')}}" >
+      <form method="post" enctype="multipart/form-data" action="{{route('product.store')}}"  >
         {{ csrf_field() }}
       <div class="modal-header">
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
@@ -12,7 +12,7 @@
         @include('product.create')
       </div>
       <div class="modal-footer">
-        <input type="submit" value="Post" class="btn btn-outline-danger btn-block button-prevent-multiple-submits">
+        <input type="submit" onclick="this.disabled=true;this.value='Submitting...'; this.form.submit();" value="Post" class="btn btn-outline-danger btn-block button-prevent-multiple-submits">
     </div>
       </form>
     </div>
