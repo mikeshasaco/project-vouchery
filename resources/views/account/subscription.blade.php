22d19ff176fbbb99c90b60c40e77f98f13f9af28
Michael Shasanya <shasanyam1@students.rowan.edu>
2021-02-27 10:53:03 -0500

    updated the current subscription prices for when a merchant create a subscription

--- resources/views/account/subscription.blade.php
+++ resources/views/account/subscription.blade.php
@@ -40,11 +40,27 @@
                                 <div>
                                     <select name="subscription_price" id="subscription_price" class="form-control select2 select2-hidden-accessible">
                                         <option value="" data-select2-id="288">Please select...</option>
-                                        <option value="5.00">$5.00</option>
+                                         <option value="1.00">$1.00</option>
+                                         <option value="2.00">$2.00</option>
+                                         <option value="3.00">$3.00</option>
+                                         <option value="4.00">$4.00</option>
+                                         <option value="5.00">$5.00</option>
+                                         <option value="6.00">$6.00</option>
+                                         <option value="7.00">$7.00</option>
+                                         <option value="8.00">$8.00</option>
+                                         <option value="9.00">$9.00</option>
                                         <option value="10.00">$10.00</option>
+                                        <option value="11.00">$11.00</option>
+                                        <option value="12.00">$12.00</option>
+                                        <option value="13.00">$13.00</option>
+                                        <option value="14.00">$14.00</option>
                                         <option value="15.00">$15.00</option>
+                                        <option value="16.00">$16.00</option>
+                                        <option value="17.00">$17.00</option>
+                                        <option value="18.00">$18.00</option>
+                                        <option value="19.00">$19.00</option>
                                         <option value="20.00">$20.00</option>
-                                        <option value="30.00">$30.00</option>
+                                         {{-- <option value="30.00">$30.00</option> --}}
                                     </select>
                                         @if ($errors->has('subscription_price'))
                                             <small class="text-danger">{{ $errors->first('subscription_price') }}</small>
